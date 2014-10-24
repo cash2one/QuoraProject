660d77042f4c8df4e9f5107ab02d4943b519b98e
empathetic-alligator <willipovell@gmail.com>
2014-10-23 21:20:09 -0400

    Added script to scrape quora pages and save them as hex-encoded gzip-compressed strings.

---
+++ scrapePages.py
@@ -0,0 +1,31 @@
+import scrape
+from time import time
+from lxml.etree import tostring
+import json
+import gzip
+import binascii
+import logging
+
+if __name__ == '__main__':
+	output = []
+	try:
+		for i, q in enumerate(scrape.getQuestionPage()):
+			scrape.driver.delete_all_cookies()
+			print("{} : {}".format(i, q))
+			data = scrape.processUrl(q)
+			if data is None:
+				print("\t(SKIPPED)")
+			else:
+				data = data('.grid_page_center_col')
+				if len(data) == 0:
+					print("\t(SKIPPED)")
+					scrape.logError(q)
+				else:
+					output.append({
+						'html' : binascii.b2a_hex(gzip.compress(tostring(data[0]))).decode('utf-8'),
+						'time' : time()
+					})
+	except KeyboardInterrupt:
+		pass
+	with open('data/scraped_{}.json'.format(int(time())), 'w') as f:
+		json.dump(output, f)
