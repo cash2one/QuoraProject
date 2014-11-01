5b2faecd1d55ef5cf2c3ba278e4d9425d29bd2f5
empathetic-alligator <willipovell@gmail.com>
2014-11-01 11:23:16 -0400

    Added binary flag to start_clients.sh

--- start_clients.sh
+++ start_clients.sh
@@ -5,5 +5,5 @@ NUM_TO_START=$3
 echo "Starting $NUM_TO_START clients sending requests to $HOST:$PORT"
 
 qsub -N ScrapeClient -l mem_free=1G,ram_free=1G -t 1-$NUM_TO_START \
--j y -o /export/a04/wpovell/logs -cwd -S /bin/bash \
+-j y -b y -o /export/a04/wpovell/logs -cwd -S /bin/bash \
 "python -u ScrapeClient.py $HOST $PORT"
