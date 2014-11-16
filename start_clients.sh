1a943723823063f42c087848899e9ef6f1eb509b
Will Povell <wpovell@x48.clsp.jhu.edu>
2014-11-16 16:24:52 -0500

    Client now sends an email when it dies.

--- start_clients.sh
+++ start_clients.sh
@@ -7,4 +7,5 @@ NUM_TO_START=$3
 echo "Starting $NUM_TO_START clients sending requests to $HOST:$PORT"
 
 qsub -N ScrapeClient -l mem_free=1G,ram_free=1G -t 1-$NUM_TO_START \
+-M willipovell@gmail.com -m eas \
 -j y -v HOST="$HOST",PORT="$PORT" -o /export/a04/wpovell/logs -cwd -S /bin/bash start.sh
