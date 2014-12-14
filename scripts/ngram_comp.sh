3554faaa4f0c1e0921b3dd56205d7aada2430535
empathetic-alligator <willipovell@gmail.com>
2014-12-14 17:55:25 -0500

    Fixed typo in ngram qsub script.

--- scripts/ngram_comp.sh
+++ scripts/ngram_comp.sh
@@ -4,6 +4,6 @@
 #$ -N NGramComparison
 #$ -S /bin/bash
 #$ -j y -o /export/a04/wpovell/logs
-#$ -l mem_free3G,ram_free=3G
+#$ -l mem_free=3G,ram_free=3G
 
 python -u -m Quora.exp.ngram_comp -q /export/a04/wpovell/scrape_data_ordered -t /export/a04/fferraro/twitter/rand10/2013/02/2013_02_14_05_24_31.gz
