6231368d114d0b4b2dc65e79dcb0b49a7f1aeb8c
empathetic-alligator <willipovell@gmail.com>
2015-01-05 21:36:17 -0500

    Fixed typos in scripts.

--- scripts/start_clients.sh
+++ scripts/start_clients.sh
@@ -8,4 +8,4 @@ echo "Starting $NUM_TO_START clients sending requests to $HOST:$PORT"
 
 qsub -N ScrapeClient -l mem_free=1G,ram_free=1G -t 1-$NUM_TO_START \
 -M willipovell@gmail.com -m eas \
--j y -v HOST="$HOST",PORT="$PORT" -o /export/a04/wpovell/logs -cwd -S /bin/bash start.sh
+-j y -v HOST="$HOST",PORT="$PORT" -o /export/a04/wpovell/logs -cwd -S /bin/bash scripts/start.sh
