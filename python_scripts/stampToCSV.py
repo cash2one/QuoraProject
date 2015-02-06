adda7c9404b7ac13efe4287ac5059228964987f6
empathetic-alligator <willipovell@gmail.com>
2015-02-05 19:04:16 -0500

    Fixed bug in stampToCSV

--- python_scripts/stampToCSV.py
+++ python_scripts/stampToCSV.py
@@ -3,8 +3,10 @@ from datetime import datetime
 with open('/export/a04/wpovell/logs/CreateQuoraComms.o1319643') as f:
 	data = f.read().split('\n')
 
-with open('/export/a04/wpovell/quora_times.csv') as f:
+with open('/export/a04/wpovell/quora_times.csv', 'w') as f:
 	for n in data:
+		if not n:
+			continue
 		dt = datetime.fromtimestamp(int(n))
 		s = dt.strftime("%Y,%m,%d\n")
 		f.write(s)
