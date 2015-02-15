7077f91e874b7b65bb2459df98bf26827c9db1a6
empathetic-alligator <willipovell@gmail.com>
2015-02-15 12:24:49 -0500

    Updated jar path.

--- JavaAnnotation/annotateDir.sh
+++ JavaAnnotation/annotateDir.sh
@@ -1,3 +1,3 @@
-DIR=head -n $SGE_TASK_ID /export/a04/wpovell/fileList.txt | tail -n 1
-java -cp .:/home/ferraro/code/concrete-stanford/target/concrete-stanford-3.10.3-jar-with-dependencies.jar StanfordAnnotationTool "/export/a04/wpovell/hashed_data" "/export/a04/wpovell/annotated_data" $DIR
+DIR=`head -n $SGE_TASK_ID /export/a04/wpovell/fileList.txt | tail -n 1`
+java -cp .:concrete-stanford-4.2.1-jar-with-dependencies.jar StanfordAnnotationTool "/export/a04/wpovell/hashed_data" "/export/a04/wpovell/annotated_data" $DIR
 echo "DONE"
