dc339657a52ab64989283d2cdcfdc8627303ca3f
empathetic-alligator <willipovell@gmail.com>
2015-03-06 20:32:39 -0500

    Better accounting for bad files in process.py

--- Quora/process.py
+++ Quora/process.py
@@ -156,6 +156,8 @@ if __name__ == '__main__':
 		for fn, fullHash in fileList:
 			with open(fn) as f:
 				data = json.load(f)
+			if 'question' not in data:
+				continue
 			t = data['time']
 			html = data['html']
 			html = a2b_hex(html)
