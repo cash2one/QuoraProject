2653032b115ae73a6ccd64f8c121cfc637e9781a
empathetic-alligator <willipovell@gmail.com>
2015-03-07 19:39:01 -0500

    Changed JavaAnnotation scripts to write to /tmp/lwork.

--- JavaAnnotation/qsub.sh
+++ JavaAnnotation/qsub.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir /export/a04/wpovell/annotated_data
 N=`cat /export/a04/wpovell/fileList.txt | wc -l`
-qsub -N AnnotateData -cwd -l mem_free=10G,ram_free=10G -t 1-$N \
+qsub -N AnnotateData -cwd -l mem_free=5G,ram_free=5G -t 1-$N \
 -l 'arch=*64'  -v PATH -j y -o /export/a04/wpovell/logs -cwd -S /bin/bash annotateDir.sh
