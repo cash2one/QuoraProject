96fcc0bbc4bbc698af0e60961cc06bf66cc4aa82
empathetic-alligator <willipovell@gmail.com>
2015-03-09 09:03:09 -0400

    Added feature generation options.

--- gen.py
+++ gen.py
@@ -1,12 +1,62 @@
 #!/usr/bin/env python
+import json
+import hashlib
+import tarfile
+import os
+
+from concrete import Communication
+from thrift import TSerialization
+from thrift.protocol import TCompactProtocol
+
+def getID(url):
+	'''Generate url for ID'''
+	return hashlib.md5(url).hexdigest()
+
+def getFiles(d):
+	'''Recursively lists files in directory'''
+	for i in os.walk(d):
+		for j in i[2]:
+			yield '{}/{}'.format(i[0], j)
+
+def getDataFiles(data):
+	'''Open all tar.gz files and return member data'''
+	for fn in getFiles("{}/data".format(data)):
+		if not fn.endswith(".tar.gz"):
+			continue
+		f = tarfile.open(fn, "r:gz")
+		for tarfn in f.getmembers():
+			tarf = f.extractfile(tarfn)
+			yield (tarfn.name, tarf.read())
+			tarf.close()
+		f.close()
+
+def commFromData(data):
+	'''Create Communication object from byte string'''
+	comm = Communication()
+	TSerialization.deserialize(comm, data, protocol_factory=TCompactProtocol.TCompactProtocolFactory())
+	return comm
 
 def followers(data):
 	'''Generate feature file for number of followers a question has'''
-	pass
+	outFile = open("{}/features/followers.txt".format(data), 'w')
+	for name, content in getDataFiles(data):
+		if not name.endswith("metadata.json"):
+			continue
+		content = json.loads(content)
+		ID = getID(content['url'])
+		outFile.write("{}: {}\n".format(ID, content["followers"]))
+	outFile.close()
 
 def question_length(data):
 	'''Generates feature file for length of question'''
-	pass
+	outFile = open("{}/features/question_length.txt".format(data), 'w')
+	for name, content in getDataFiles(data):
+		if not name.endswith("question.comm"):
+			continue
+		comm = commFromData(content)
+		ID = comm.id
+		outFile.write("{}: {}\n".format(ID, len(comm.text)))
+	outFile.close()
 
 if __name__ == '__main__':
 	import argparse
