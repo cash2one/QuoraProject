dbd41c7fd908458ca5dca53d05665ad47d538ee2
empathetic-alligator <willipovell@gmail.com>
2015-03-11 23:32:41 -0400

    Forced move in process.sh

--- scripts/process.sh
+++ scripts/process.sh
@@ -6,5 +6,5 @@
 #$ -j y -o /export/a04/wpovell/logs
 
 python -u -m Quora.process -i /export/a04/wpovell/scrape_data_ordered -o /tmp/lwork/compressed_data -m /tmp/lwork/missingTimes.txt
-mv /tmp/lwork/compressed_data /export/a04/wpovell/compressed_data
-mv /tmp/lwork/missingTimes.txt /export/a04/wpovell/missingTimes.txt
+mv -f /tmp/lwork/compressed_data /export/a04/wpovell/compressed_data
+mv -f /tmp/lwork/missingTimes.txt /export/a04/wpovell/missingTimes.txt
