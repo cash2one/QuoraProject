7ea15b27823dbb0e2f7b8bcc0e6249b10b852beb
empathetic-alligator <willipovell@gmail.com>
2015-04-10 13:10:50 -0400

    Minor changes to args for stampToCSV, fixed dir move bug in process.

--- python_scripts/stampToCSV.py
+++ python_scripts/stampToCSV.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from sys import argv
 
-with open('/export/a04/wpovell/logs/CreateQuoraComms.o1319643') as f:
+with open(argv[1]) as f:
 	data = f.read().split('\n')
 
 with open('/export/a04/wpovell/quora_times.csv', 'w') as f:
 	for n in data:
-		if not n:
+		if not n or not n.isdigit():
 			continue
 		dt = datetime.fromtimestamp(int(n))
 		s = dt.strftime("%Y,%m,%d\n")
