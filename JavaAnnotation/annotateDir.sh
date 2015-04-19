2e1d2694b57d25b3b33d655a66d44f76cf3a2d1d
empathetic-alligator <willipovell@gmail.com>
2015-04-18 23:56:17 -0400

    Fixed issue with tmp dirs for JavaAnnotation
    Previously, if two jobs ran on the same machine, they would both write to /tmp/lwork/annotated_data. The first one to finish would copy the directory and then delete it, even if the other job hadn't finished yet. Now, each job writes to its own directory /tmp/lwork/annotated_data_$SGE_JOB_ID

--- JavaAnnotation/annotateDir.sh
+++ JavaAnnotation/annotateDir.sh
@@ -1,7 +1,8 @@
 DIR=`head -n $SGE_TASK_ID /export/a04/wpovell/fileList.txt | tail -n 1`
 echo $DIR
-mkdir -p /tmp/lwork/annotated_data
-java -cp .:commons-cli-1.2.jar:concrete-stanford-4.2.1-jar-with-dependencies.jar AnnotationTool -i "/export/a04/wpovell/compressed_data" -o "/tmp/lwork/annotated_data" -f $DIR
-cp -r /tmp/lwork/annotated_data/* /export/a04/wpovell/annotated_data
-rm -r /tmp/lwork/annotated_data
+OUT_DIR="/tmp/lwork/annotated_data_$SGE_TASK_ID"
+mkdir -p $OUT_DIR
+java -cp .:commons-cli-1.2.jar:concrete-stanford-4.2.1-jar-with-dependencies.jar AnnotationTool -i "/export/a04/wpovell/compressed_data" -o "$OUT_DIR" -f $DIR
+cp -r $OUT_DIR/* /export/a04/wpovell/annotated_data
+rm -r $OUT_DIR
 echo "DONE"
