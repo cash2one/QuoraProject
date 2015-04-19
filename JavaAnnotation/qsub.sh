34459bd037264f2f2d518ab0d32ee176425ed148
empathetic-alligator <willipovell@gmail.com>
2015-04-18 22:32:08 -0400

    Limited qsub.sh to running max 10 threads at once.

--- JavaAnnotation/qsub.sh
+++ JavaAnnotation/qsub.sh
@@ -2,4 +2,4 @@
 mkdir /export/a04/wpovell/annotated_data
 N=`cat /export/a04/wpovell/fileList.txt | wc -l`
 qsub -N AnnotateData -cwd -l mem_free=5G,ram_free=5G -t 1-$N \
--l 'arch=*64'  -v PATH -j y -o /export/a04/wpovell/logs -cwd -S /bin/bash annotateDir.sh
+-l 'arch=*64' -tc 10 -v PATH -j y -o /export/a04/wpovell/logs -cwd -S /bin/bash annotateDir.sh
