514dc25475af591d085e58032c31a882b1d1b03b
empathetic-alligator <willipovell@gmail.com>
2015-04-18 23:14:48 -0400

    Ensure lwork/annotated_data exists.

--- JavaAnnotation/annotateDir.sh
+++ JavaAnnotation/annotateDir.sh
@@ -1,5 +1,6 @@
 DIR=`head -n $SGE_TASK_ID /export/a04/wpovell/fileList.txt | tail -n 1`
 echo $DIR
+mkdir -p /tmp/lwork/annotated_data
 java -cp .:commons-cli-1.2.jar:concrete-stanford-4.2.1-jar-with-dependencies.jar AnnotationTool -i "/export/a04/wpovell/compressed_data" -o "/tmp/lwork/annotated_data" -f $DIR
 cp -r /tmp/lwork/annotated_data/* /export/a04/wpovell/annotated_data
 rm -r /tmp/lwork/annotated_data
