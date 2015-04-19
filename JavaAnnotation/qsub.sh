a80f4805056f17b6c20ae08e81ee15bbc021ca51
empathetic-alligator <willipovell@gmail.com>
2015-04-19 12:27:54 -0400

    Fixed memory limits.

--- JavaAnnotation/qsub.sh
+++ JavaAnnotation/qsub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /export/a04/wpovell/annotated_data
 N=`cat /export/a04/wpovell/fileList.txt | wc -l`
-qsub -N AnnotateData -cwd -l mem_free=5G,ram_free=5G -t 1-$N \
+qsub -N AnnotateData -cwd -l mem_free=50G,ram_free=50G -t 1-$N \
 -l 'arch=*64' -tc 10 -v PATH -j y -o /export/a04/wpovell/logs -cwd -S /bin/bash annotateDir.sh
