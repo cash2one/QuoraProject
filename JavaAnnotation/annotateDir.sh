eb3f1717da6adf887ced1bbf08a9fb2f9e057219
empathetic-alligator <willipovell@gmail.com>
2015-04-20 18:40:21 -0400

    Updated java annotation scripts to prevent mem errors and improve debugging.

--- JavaAnnotation/annotateDir.sh
+++ JavaAnnotation/annotateDir.sh
@@ -1,8 +1,17 @@
 DIR=`head -n $SGE_TASK_ID /export/a04/wpovell/fileList.txt | tail -n 1`
-echo $DIR
+HOST=$(hostname)
+echo "DIR=$DIR"
+echo "HOST=$HOST"
 OUT_DIR="/tmp/lwork/annotated_data_$SGE_TASK_ID"
 mkdir -p $OUT_DIR
-java -cp .:commons-cli-1.2.jar:concrete-stanford-4.2.1-jar-with-dependencies.jar AnnotationTool -i "/export/a04/wpovell/compressed_data" -o "$OUT_DIR" -f $DIR
-cp -r $OUT_DIR/* /export/a04/wpovell/annotated_data
+java -cp .:commons-cli-1.2.jar:concrete-stanford-4.2.1-jar-with-dependencies.jar AnnotationTool -i "/export/a04/wpovell/compressed_data" -o $OUT_DIR -f $DIR;
+EXIT_CODE=$?
+if [ $EXIT_CODE -eq 1 ]; then
+    echo "Job $JOB_ID.$SGE_TASK_ID failed on machine $HOST processing dir $DIR"
+    echo $_ | mail -s "JOB ERROR" willipovell@gmail.com
+else
+    cp -r $OUT_DIR/* /export/a04/wpovell/annotated_data
+fi
+
 rm -r $OUT_DIR
 echo "DONE"
--- JavaAnnotation/qsub.sh
+++ JavaAnnotation/qsub.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir /export/a04/wpovell/annotated_data
 N=`cat /export/a04/wpovell/fileList.txt | wc -l`
-qsub -N AnnotateData -cwd -l mem_free=50G,ram_free=50G -t 1-$N \
--l 'arch=*64' -tc 10 -v PATH -j y -o /export/a04/wpovell/logs -cwd -S /bin/bash annotateDir.sh
+qsub -N AnnotateData -cwd -l mem_free=50G,ram_free=50G,h_vmem=50G,arch=*64 -t 1-$N \
+-tc 10 -v PATH -j y -o /export/a04/wpovell/logs -S /bin/bash annotateDir.sh
+
