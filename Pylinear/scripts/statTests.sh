fe424fd3f59851720e7164dfb375773fec9f71ec
empathetic-alligator <willipovell@gmail.com>
2015-05-12 14:07:56 -0400

    Updated mem requirements for statTests.

--- Pylinear/scripts/statTests.sh
+++ Pylinear/scripts/statTests.sh
@@ -4,6 +4,5 @@
 #$ -N statTests
 #$ -S /bin/bash
 #$ -j y -o /export/a04/wpovell/logs
-#$ -l mem_free=60G,ram_free=60G
-
-python -u -m Pylinear.scripts.statTests /export/a04/wpovell/splits/test/data
+#$ -l mem_free=30G,ram_free=30G,h_vmem=30G
+python -u -m Pylinear.scripts.statTests /export/a04/wpovell/splits/train/data
