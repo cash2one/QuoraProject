3c8144f3687a7cf39e0a4e5566113a7d66fa15e1
empathetic-alligator <willipovell@gmail.com>
2015-05-11 22:11:12 -0400

    Typos on typos on typos.

--- Pylinear/scripts/statTests.py
+++ Pylinear/scripts/statTests.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 	lastThread = ""
 	numAnswers = 0
 	first = True
-	hastDetails = False
+	hasDetails = False
 	for n, f in getDataFiles(DIR):
 		thread = n.split("/")[1]
 		if first:
