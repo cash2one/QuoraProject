b959aac3083a296cad25b64d79cf679580246ef9
empathetic-alligator <willipovell@gmail.com>
2015-07-07 20:10:38 -0400

    Added script to calculate precision, recall, and F1 for predicitions.

---
+++ Pylinear/scripts/scores.py
@@ -0,0 +1,37 @@
+from __future__ import division
+
+def FMeasure(pres, recall, beta=1):
+	(1 + beta**2) * pres * recall / (beta**2 * pres + recall)
+
+if __name__ == '__main__':
+	from sys import argv
+	real = open(argv[0])
+	pred = open(argv[1])
+	realLine = real.readline().split()[0].strip()
+	predLine = pred.readline().strip()
+	total = 0
+	tp    = 0
+	tn    = 0
+	fp    = 0
+	fn    = 0
+	while realLine:
+		total += 1
+		if realLine == predLine:
+			if predLine == '1':
+				tp += 1
+			else:
+				tn += 1
+		else:
+			if predLine == '1':
+				fp += 1
+			else:
+				fn += 1
+		realLine = real.readline().split()[0].strip()
+		predLine = pred.readline().strip()
+
+	pres   = tp / (tp + fp)
+	recall = tp / (tp + fn)
+	print("Accuracy   {}%".format((tp + tn)) / total)
+	print("Precision  {}%".format(pres))
+	print("Recall     {}%".format(recall))
+	print("F1         {}%".format(FMeasure(pres, recall)))
