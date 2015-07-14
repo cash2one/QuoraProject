3c88e41e3328cdf426203d928bc0e3b703828e83
empathetic-alligator <willipovell@gmail.com>
2015-07-14 14:25:22 -0400

    Added script to remove threads with no question.comm

---
+++ Pylinear/scripts/removeBadThreads.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+threads="709f97e52894d9843f5b6523b8bc481f
+9aaa149fbfbd1aae1e0c2d0352c54316
+93b6185d1bae094601a92484882ee227
+3e3d7bb31dfe3348fc4586adbd1594bd
+4c5787ad7d686f2fc7e3dfa6ec33cdcb
+40a7243d8750a68441f791a62182f24f
+4e78b5b4e25d9b578500f7b004cdbaab"
+
+rmFromArchive() {
+	echo "gunzip $1"
+	gunzip $1
+	echo "tar --delete --file=${1:0:-3} ${2:2:1}/$2"
+	tar --delete --file=${1:0:-3} ${2:2:1}/$2
+	echo "gzip ${1:0:-3}"
+	gzip ${1:0:-3}
+}
+
+for i in $threads; do
+	file=${i:0:1}/${i:1:1}/${i:2:1}.tar.gz
+	thread=${i:2:1}/$i
+	rmFromArchive /export/a04/wpovell/compressed_data/$file $thread
+	rmFromArchive /export/a04/wpovell/annotated_data/$file $thread
+done;
