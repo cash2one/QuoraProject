c4c1d0bcb35806929d068fcbb0a3d18e2fa37278
empathetic-alligator <willipovell@gmail.com>
2015-07-27 00:14:40 -0400

    Made scrapeLogs workable on non-CLSP grid.
    Allows base path for output to be set using the SCRAPE_LOG_BASEPATH environment variable.

--- Quora/util/scrapeLogs.py
+++ Quora/util/scrapeLogs.py
@@ -5,13 +5,18 @@ import gzip
 from StringIO import StringIO
 import binascii
 import urllib2
+import os
 import selenium.common
 
-with open('/export/a04/wpovell/scrapedUrls.txt') as f:
+basePath = '/export/a04/wpovell/'
+if 'SCRAPE_LOG_BASEPATH' in os.environ:
+    basePath = os.environ['SCRAPE_LOG_BASEPATH']
+
+with open(os.path.join(basePath, 'scrapedUrls.txt')) as f:
     inp = set(f.read().strip().split('\n'))
-with open('/export/a04/wpovell/rescrapedUrls.txt') as f:
+with open(os.path.join(basePath, 'rescrapedUrls.txt')) as f:
     done = set(f.read().strip().split('\n'))
-doneF = open('/export/a04/wpovell/rescrapedUrls.txt', 'a')
+doneF = open(os.path.join(basePath, 'rescrapedUrls.txt'), 'a')
 
 qs = QuoraScraper()
 for line in list(inp - done):
@@ -31,7 +36,7 @@ for line in list(inp - done):
     compressed_html = out.getvalue()
     compressed_html = binascii.b2a_hex(compressed_html).decode('utf-8')
     fn = hashlib.md5(line.encode('utf-8')).hexdigest()
-    with open('/export/a04/wpovell/logPages/{}'.format(fn), 'w') as f:
+    with open(os.path.join(basePath, 'logPages/{}'.format(fn)), 'w') as f:
         json.dump({
             'html' : compressed_html,
             'url'  : line
